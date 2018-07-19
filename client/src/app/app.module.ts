42a40ad4a066e2aa2635fb6cdb9dd6298c42bdc0
anupam <contact@drafteq.com>
2018-07-19 19:56:23 +0530

    initial commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ChartModule } from 'angular2-highcharts';
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
+
+import { AppComponent } from './app.component';
+
+export function highchartsFactory() {
+  return require('highcharts');
+}
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ChartModule
+  ],
+  providers: [{
+    provide: HighchartsStatic,
+    useFactory: highchartsFactory
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
